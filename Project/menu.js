eaeb1eebcbbeefc06ce846b96b79ffa483d52c59
Oleh Korsun <oleg.korsun.0505@gmail.com>
2025-11-04 13:50:32 +0100

    Separating footer into a separate file and connecting in with a script menu.js. Styles for registration form.

--- Project/menu.js
+++ Project/menu.js
@@ -4,13 +4,25 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(data => {
             document.getElementById("placeForNavbar").innerHTML = data;
 
-            // подсвечиваем активный пункт меню
-            const links = document.querySelectorAll("nav a");
-            links.forEach(link => {
-                if (link.href === window.location.href) {
-                    link.classList.add("active");
-                }
-            });
+            // gdybym chciał podświetlić w menu na której stronie jestem
+            // const links = document.querySelectorAll("nav a");
+            // links.forEach(link => {
+            //     if (link.href === window.location.href) {
+            //         link.classList.add("active");
+            //     }
+            // });
+
         })
         .catch(error => console.error("Nie udało się załadować menu:", error));
+
+
+
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("placeForFooter").innerHTML = data;
+
+
+        })
+        .catch(error => console.error("Nie udało się załadować footera:", error));
 });
